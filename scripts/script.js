eaf2c7c996bf0e0ee78776ac123289551be94ad6
Emily Klaebe <emklaebe@pdx.edu>
2019-11-07 07:39:05 -0800

    editing imges and content main page

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,20 @@ $('h4').on('click', function (){
 	$('.hide').css('display', 'block')
 })
 
+$('body').flowtype({
+	 minimum : 500,
+	 maximum : 1200,
+	 minFont : 12,
+	 maxFont : 90,
+});
+
+$('h1' 'h2' 'h3' 'h4').flowtype({
+	 minimum : 500,
+	 maximum : 1200,
+	 minFont : 18,
+	 maxFont : 100,
+});
+
 
 
 
