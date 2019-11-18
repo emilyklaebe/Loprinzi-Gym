69a8f5bc2edfc7d565bd80523753c917c94b7dbb
Emily Klaebe <emklaebe@pdx.edu>
2019-11-18 15:32:26 -0800

    updates script.js

--- scripts/script.js
+++ scripts/script.js
@@ -29,42 +29,7 @@ $( ".group-rates" ).click(function() {
 
 // Focus Point Test Below
 
-$(document).ready(function() {
-		
-		$(".joe").click(function(e){
-		
-			var imageW = $(this).width();
-			var imageH = $(this).height();
-			
-			//Calculate FocusPoint coordinates
-			var offsetX = e.pageX - $(this).offset().left;
-			var offsetY = e.pageY - $(this).offset().top;
-			var focusX = (offsetX/imageW - .5)*2;
-			var focusY = (offsetY/imageH - .5)*-2;
-			
-			//Calculate CSS Percentages
-			var percentageX = (offsetX/imageW)*100;
-			var percentageY = (offsetY/imageH)*100;
-			var backgroundPosition = percentageX.toFixed(0) + '% ' + percentageY.toFixed(0) + '%';
-			var backgroundPositionCSS = 'background-position: ' + backgroundPosition + ';';
-			
-			window.alert('FocusX:' + focusX.toFixed(2) + ', FocusY:' + focusY.toFixed(2) + ' (For CSS version: ' + backgroundPositionCSS + ')');
-		
-});
 
-
-	var setupContainer = function($el) {
-		var imageSrc = $el.find('img').attr('src');
-		$el.data('imageSrc', imageSrc);
-
-		resolveImageSize(imageSrc, function(err, dim) {
-			$el.data({
-				imageW: dim.width,
-				imageH: dim.height
-			});
-			adjustFocus($el);
-		});
-	};
 
 
 
