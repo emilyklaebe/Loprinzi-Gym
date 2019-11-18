7c7e58b3935e572e9916b49e9e7ec6d0b86d73b6
Emily Klaebe <emklaebe@pdx.edu>
2019-11-18 14:50:34 -0800

    playing with contracts section (not done!)

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,13 @@
 // Your Code from here on down. Don't delete that line above!
   
 
-// $('.standard-rates').on('click', function (){
-//	$('.hide').css('display', '.table-rates')
-// })
+// $(document).scroll(function () {
+//   var $nav = $(".navbar-fixed-top");
+//   $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+// });
 
+
+// Drop Downs
 
 var flip = 0;
 $( ".standard-rates" ).click(function() {
@@ -23,11 +26,45 @@ $( ".group-rates" ).click(function() {
   $( ".table-three" ).toggle( flip++ % 2 === 0 ).slideToggle("slow");
 }); 
 
-$(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+
+// Focus Point Test Below
+
+$(document).ready(function() {
+		
+		$(".joe").click(function(e){
+		
+			var imageW = $(this).width();
+			var imageH = $(this).height();
+			
+			//Calculate FocusPoint coordinates
+			var offsetX = e.pageX - $(this).offset().left;
+			var offsetY = e.pageY - $(this).offset().top;
+			var focusX = (offsetX/imageW - .5)*2;
+			var focusY = (offsetY/imageH - .5)*-2;
+			
+			//Calculate CSS Percentages
+			var percentageX = (offsetX/imageW)*100;
+			var percentageY = (offsetY/imageH)*100;
+			var backgroundPosition = percentageX.toFixed(0) + '% ' + percentageY.toFixed(0) + '%';
+			var backgroundPositionCSS = 'background-position: ' + backgroundPosition + ';';
+			
+			window.alert('FocusX:' + focusX.toFixed(2) + ', FocusY:' + focusY.toFixed(2) + ' (For CSS version: ' + backgroundPositionCSS + ')');
+		
 });
 
+
+	var setupContainer = function($el) {
+		var imageSrc = $el.find('img').attr('src');
+		$el.data('imageSrc', imageSrc);
+
+		resolveImageSize(imageSrc, function(err, dim) {
+			$el.data({
+				imageW: dim.width,
+				imageH: dim.height
+			});
+			adjustFocus($el);
+		});
+	};
 
 
 
